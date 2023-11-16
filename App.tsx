de44daf67211edc5a9d500f63b6fc6f35b9840b2
Adam Horodyski <ad.horodyski@gmail.com>
2023-11-16 14:44:49 +0100

    fix: native builds

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Text, Platform } from "react-native";
+import { StyleSheet, View, Text, Platform, SafeAreaView } from "react-native";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./src/lib/queryClient";
 import { PostsList } from "./src/PostsList";
@@ -8,7 +8,7 @@ import { CreatePost } from "./src/CreatePost";
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <View style={styles.container}>
+      <SafeAreaView style={styles.container}>
         <Text style={styles.platform}>Platform: {Platform.OS}</Text>
 
         <View style={styles.actionWrapper}>
@@ -16,9 +16,9 @@ export default function App() {
         </View>
 
         <PostsList />
-      </View>
 
-      <StatusBar style="auto" />
+        <StatusBar style="auto" />
+      </SafeAreaView>
     </QueryClientProvider>
   );
 }
